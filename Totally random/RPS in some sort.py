e3c6b812f4f466e5beaac565d0495deef11f4732
david <89749147+analnyroztahovac@users.noreply.github.com>
2023-01-15 14:02:07 +0100

    cleanup

--- Totally random/RPS in some sort.py
+++ Totally random/RPS in some sort.py
@@ -1,34 +1,35 @@
-def rock_paper_scissors():
+def rock_paper_scissors() -> None:
     import random
 
-    player_score = 0
-    bot_score = 0
-    print('Pravidlá: \n 1. kys \n 2. ma rock paper scissors aj nejake pravidla? \n 3. AK CHCES UKONCIT HRU SKOREJ NAPIS "END"')
+    player_score, bot_score = 0, 0
+    print("Pravidlá:\n "
+          "1. kys\n "
+          "2. ma rock paper scissors aj nejake pravidla?\n"
+          "3. AK CHCES UKONCIT HRU SKOREJ NAPIS \"END\"")
     total_rounds = int(input("How many rounds do you want to play: "))
 
-    for round in range(total_rounds):
-        player = input()
+    for kolo in range(1, total_rounds + 1):
+        player = input().lower()
         bot = random.choice(["rock", "paper", "scissors"])
 
         if player == "rock" and bot == "scissors" or player == "paper" and bot == "rock" or player == "scissors" and bot == "paper":
-            print(round + 1, "round wins Player!")
+            print(kolo, "round wins Player!")
             player_score += 1
 
         elif bot == "rock" and player == "scissors" or bot == "paper" and player == "rock" or bot == "scissors" and player == "paper":
-            print(round + 1, "round wins Bot!")
+            print(kolo, "round wins Bot!")
             bot_score += 1
 
         elif player == bot:
-            print(round + 1, "round is a draw!")
+            print(kolo, "round is a draw!")
 
         elif player == "end":
             break
 
         else:
-            pass
             print("Invalid input.")
 
 
-    print("\nNahral si ", player_score, "bodov! \nBot nahral ", bot_score, "bodov!")
+    print(f"Nahral si {player_score} bodov! \nBot nahral {bot_score} bodov!")
 
 rock_paper_scissors()
