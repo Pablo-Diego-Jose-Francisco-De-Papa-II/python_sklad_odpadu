5a703b68660fa02a595121990276d4642945a985
aksel <114018219+Pablo-Diego-Jose-Francisco-De-Papa-II@users.noreply.github.com>
2023-01-15 14:24:50 +0100

    Update Hangman.py

--- Totally random/Hangman.py
+++ Totally random/Hangman.py
@@ -1,6 +1,7 @@
-def hangman():
+def hangman(base, pole, upper_wood, support, rope, end, head, body, left_arm, right_arm, left_leg, right_leg):
     import random
-    guessing_word = ["Body", "soil", "owl", "fit", "cup", "helmet", "marsh", "smash", "career", "warm", "discipline", "lean", "suffering", "quality", "population", "harmony", "eagle", "feather", "origin", "manage", "reproduction", "scatter", "certain", "healthy", "dismiss", "introduce", "flock", "pasture", "office", "attractive", "cheap", "linen", "relieve", "habitat", "cassette", "difference", "strain", "lift", "operational", "gasp", "environmental", "soup", "colorblind", "chain", "feminine", "shoot", "tank", "rock", "abbey", "mosquito", "zoophobia", "mario je gay", "reluctance" "abundant" "strange" "expenditure" "trench" "execution" "dollar" "monster" "community" "factor" "functional", "vegetable", "abolish", "falsify", "memorandum", "forestry", "evening", "residence", "glasses", "calculation", "favour", "production", "requirement", "continuation", "partnership", "implication", "preoccupation", "personality", "headquarters", "performance", "association"]
+
+    guessing_word = ["Body", "soil", "owl", "fit", "cup", "helmet", "marsh", "smash", "career", "warm", "discipline", "lean", "suffering", "quality", "population", "harmony", "eagle", "feather", "origin", "manage", "reproduction", "scatter", "certain", "healthy", "dismiss", "introduce", "flock", "pasture", "office", "attractive", "cheap", "linen", "relieve", "habitat", "cassette", "difference", "strain", "lift", "operational", "gasp", "environmental", "soup", "colorblind", "chain", "feminine", "shoot", "tank", "rock", "abbey", "mosquito", "zoophobia", "mario je gay", "reluctance", "abundant", "strange", "expenditure", "trench", "execution", "dollar", "monster", "community", "factor", "functional", "vegetable", "abolish", "falsify", "memorandum", "forestry", "evening", "residence", "glasses", "calculation", "favour", "production", "requirement", "continuation", "partnership", "implication", "preoccupation", "personality", "headquarters", "performance", "association"]
     num = random.randint(0, len(guessing_word))
     word = list(guessing_word[num].upper())
 
@@ -32,124 +33,41 @@ def hangman():
                         attempt += 1
 
             if attempt == 0:
-                print("+---------------+")
-                print("|               |")
-                print("|               |")
-                print("|               |")
-                print("|               |")
-                print("|               |")
-                print("|               |")
-                print("+---------------+")
-
+                pass
             elif attempt == 1:
-                print("+---------------+")
-                print("|               |")
-                print("|               |")
-                print("|               |")
-                print("|               |")
-                print("|               |")
-                print("|   A           |")
-                print("+---------------+")
-
+                base = "A"
             elif attempt == 2:
-                print("+---------------+")
-                print("|               |")
-                print("|   |           |")
-                print("|   |           |")
-                print("|   |           |")
-                print("|   |           |")
-                print("|   A           |")
-                print("+---------------+")
-
+                pole = "|"
             elif attempt == 3:
-                print("+---------------+")
-                print("|   _________   |")
-                print("|   |           |")
-                print("|   |           |")
-                print("|   |           |")
-                print("|   |           |")
-                print("|   A           |")
-                print("+---------------+")
-
+                upper_wood = "_"
             elif attempt == 4:
-                print("+---------------+")
-                print("|   _________   |")
-                print("|   |/          |")
-                print("|   |           |")
-                print("|   |           |")
-                print("|   |           |")
-                print("|   A           |")
-                print("+---------------+")
-
+                support = "/"
             elif attempt == 5:
-                print("+---------------+")
-                print("|   _________   |")
-                print("|   |/      |   |")
-                print("|   |           |")
-                print("|   |           |")
-                print("|   |           |")
-                print("|   A           |")
-                print("+---------------+")
-
+                rope = "|"
             elif attempt == 6:
-                print("+---------------+")
-                print("|   _________   |")
-                print("|   |/      |   |")
-                print("|   |       o   |")
-                print("|   |           |")
-                print("|   |           |")
-                print("|   A           |")
-                print("+---------------+")
-
+                head = "o"
             elif attempt == 7:
-                print("+---------------+")
-                print("|   _________   |")
-                print("|   |/      |   |")
-                print("|   |       o   |")
-                print("|   |       |   |")
-                print("|   |           |")
-                print("|   A           |")
-                print("+---------------+")
-
+                body = "|"
             elif attempt == 8:
-                print("+---------------+")
-                print("|   _________   |")
-                print("|   |/      |   |")
-                print("|   |       o   |")
-                print("|   |      /|   |")
-                print("|   |           |")
-                print("|   A           |")
-                print("+---------------+")
-
+                left_arm = "/"
             elif attempt == 9:
-                print("+---------------+")
-                print("|   _________   |")
-                print("|   |/      |   |")
-                print("|   |       o   |")
-                print("|   |      /|\  |")
-                print("|   |           |")
-                print("|   A           |")
-                print("+---------------+")
-
+                right_arm = "\\"
             elif attempt == 10:
-                print("+---------------+")
-                print("|   _________   |")
-                print("|   |/      |   |")
-                print("|   |       o   |")
-                print("|   |      /|\  |")
-                print("|   |      /    |")
-                print("|   A           |")
-                print("+---------------+")
-
+                left_leg = "/"
             elif attempt == 11:
-                print("+---------------+")
-                print("|   _________   |")
-                print("|   |/      |   |")
-                print("|   |  END  o   |")
-                print("|   |      /|\  |")
-                print("|   |      / \  |")
-                print("|   A           |")
-                print("+---------------+")
+                right_leg = "\\"
+                end = "END"
+
+            print("+---------------+")
+            print("|   {}{}{}{}{}{}{}{}{}   |".format(upper_wood, upper_wood, upper_wood, upper_wood, upper_wood, upper_wood, upper_wood, upper_wood, upper_wood))
+            print("|   {}{}      {}   |".format(pole, support, rope))
+            print("|   {}  {}  {}   |".format(pole, end, head))
+            print("|   {}      {}{}{}  |".format(pole, left_arm, body, right_arm))
+            print("|   {}      {} {}  |".format(pole, left_leg, right_leg))
+            print("|   {}           |".format(base))
+            print("+---------------+")
+
+            if attempt == 11:
                 print("Word you were guessing was: ", "".join([name for name in word if name.isalpha]))
                 return print(*guess)
 
@@ -157,4 +75,4 @@ def hangman():
 
     else:
         return print("Word you were guessing was indeed","".join([name for name in word if name.isalpha]), "\nGOOD JOB! YOU GUESSED IT RIGHT!")
-hangman()
+hangman(" ", " ", " ", " ", " ", "   ", " ", " ", " ", " ", " ", " ")
