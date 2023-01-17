1d03703e2c43f3e2a32045465b3f0a7c1809d6fd
aksel <114018219+Pablo-Diego-Jose-Francisco-De-Papa-II@users.noreply.github.com>
2023-01-17 22:58:40 +0100

    Update 17.01.2023.py

--- Python/17.01.2023.py
+++ Python/17.01.2023.py
@@ -7,26 +7,23 @@ canvas = tk.Canvas(root, width = 500, height = 500, bd = 10, bg = "silver")
 canvas.pack()
 
 
-def prva_uloha():
-    cords = []
+cords = []
 
-    def stvrte_cvicenie_klik(x, y):
-        global line, cords
-        color = rd.choice(["blue", "red", "green", "yellow", "cyan", "pink"])
-        cords = [x, y]
-        line = canvas.create_polygon(0, 0, 0, 0, width = 4, fill = color)
+def stvrte_cvicenie_klik(x, y):
+    global line, cords
+    color = rd.choice(["blue", "red", "green", "yellow", "cyan", "pink"])
+    cords = [x, y]
+    line = canvas.create_polygon(0, 0, 0, 0, width = 4, fill = color)
 
-    def stvrte_cvicenie_motion(x, y):
-        cords.extend([x, y])
-        canvas.coords(line, cords)
+def stvrte_cvicenie_motion(x, y):
+    cords.extend([x, y])
+    canvas.coords(line, cords)
 
-    def click(mouse):
-        stvrte_cvicenie_klik(mouse.x, mouse.y)
-        pass
+def click(mouse):
+    stvrte_cvicenie_klik(mouse.x, mouse.y)
 
-    def move(mouse):
-         stvrte_cvicenie_motion(mouse.x, mouse.y)
+def move(mouse):
+    stvrte_cvicenie_motion(mouse.x, mouse.y)
 
-    canvas.bind("<Button-1>", click)
-    canvas.bind("<B1-Motion>", move)
-prva_uloha()
+canvas.bind("<Button-1>", click)
+canvas.bind("<B1-Motion>", move)
