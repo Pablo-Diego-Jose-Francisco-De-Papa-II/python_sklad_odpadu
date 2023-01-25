a22114f839bbbf06e74733d2128ef0660678e5b8
aksel <114018219+Pablo-Diego-Jose-Francisco-De-Papa-II@users.noreply.github.com>
2023-01-25 14:34:11 +0100

    Update 24.01.2023.py

--- Python/24.01.2023.py
+++ Python/24.01.2023.py
@@ -11,7 +11,6 @@ canvas.create_line(position, tags="lajna", width=3)
 
 def prva_uloha(event):
     global position, move
-    
     if event.keysym == "Right":
         position.extend([position[-2] + move, position[-1]])
     elif event.keysym == "Left":
@@ -20,7 +19,21 @@ def prva_uloha(event):
         position.extend([position[-2], position[-1] - move])
     elif event.keysym == "Down":
         position.extend([position[-2], position[-1] + move])
-        
+
     canvas.coords("lajna", position)
 canvas.bind_all("<Key>", prva_uloha)
-canvas.mainloop()
+
+
+obr, move = tk.PhotoImage(file = "notmiguel.png"), 10
+canvas.create_image(250, 250, image = obr, tags = "gato")
+
+def kiten(key):
+    name = key.keysym
+
+    match name:
+        case "Right": canvas.move("gato", move, 0)
+        case "Left":  canvas.move("gato", -move, 0)
+        case "Up":    canvas.move("gato", 0, -move)
+        case "Down":  canvas.move("gato", 0, move)
+
+canvas.bind_all("<Key>", kiten)
