d3762be7fd97421aa64258c59589c8899fd1dce4
aksel <114018219+Pablo-Diego-Jose-Francisco-De-Papa-II@users.noreply.github.com>
2023-02-03 19:44:11 +0100

    brm brm cars

--- Python/31.01.2023.py
+++ Python/31.01.2023.py
@@ -6,7 +6,6 @@ root.title("brm brm game where you cant do anything!")
 canvas = tk.Canvas(root, width=1480, height=444)
 canvas.pack()
 
-
 red_pos = 0
 blue_pos = 0
 time = 0
@@ -20,48 +19,67 @@ canvas.create_image(red_pos, 111, image=red, tags="red_car")
 blue = tk.PhotoImage(file="blue_car_no_bg.png")
 canvas.create_image(blue_pos, 333, image=blue, tags="blue_car")
 
-while red_pos <= 1250 or blue_pos <= 1250:
-    red_move = rd.randint(1, 10)
-    blue_move = rd.randint(1, 10)
+def click(start):
+    global red_pos, red_move, blue_pos, blue_move, time
 
-    canvas.move("red_car", red_move, 0)
-    canvas.move("blue_car", blue_move, 0)
+    while red_pos <= 1250 or blue_pos <= 1250:
+        red_move = rd.randint(1, 10)
+        blue_move = rd.randint(1, 10)
 
-    red_pos += red_move
-    blue_pos += blue_move
+        canvas.move("red_car", red_move, 0)
+        canvas.move("blue_car", blue_move, 0)
 
-    time += 0.01
+        red_pos += red_move
+        blue_pos += blue_move
 
-    if red_pos <= 1250:
-        cas1 = canvas.create_text(1445, 111, text=round(time, 3), font="Arial 25 bold", fill = "white")
-        fin1 = time
+        time += 0.01
+
+        if red_pos <= 1250:
+            cas1 = canvas.create_text(1445, 111, text=round(time, 3), font="Arial 25 bold", fill="white")
+            fin1 = time
+        else:
+            canvas.create_text(1445, 111, text=round(fin1, 3), font="Arial 25 bold", fill="white")
+
+        if blue_pos <= 1250:
+            cas2 = canvas.create_text(1445, 333, text=round(time, 3), font="Arial 25 bold", fill="white")
+            fin2 = time
+        else:
+            canvas.create_text(1445, 333, text=round(fin2, 3), font="Arial 25 bold", fill="white")
+
+        canvas.update()
+        canvas.after(10)
+
+        if red_pos < 1250 or blue_pos < 1250:
+            canvas.delete(cas1)
+            canvas.delete(cas2)
     else:
-        canvas.create_text(1445, 111, text=round(fin1, 3), font="Arial 25 bold", fill="white")
+        if red_pos > blue_pos:
+            print("RED WINS!")
+            canvas.create_rectangle(440, 150, 1040, 294, fill="white", outline="#ff393a", width=10)
+            canvas.create_text(740, 222, text="RED WINS!", font="Arial 70 bold", fill="#ff393a")
 
-    if blue_pos <= 1250:
-        cas2 = canvas.create_text(1445, 333, text=round(time, 3), font="Arial 25 bold", fill = "white")
-        fin2 = time
-    else:
-        canvas.create_text(1445, 333, text=round(fin2, 3), font="Arial 25 bold", fill="white")
+        elif red_pos < blue_pos:
+            print("BLUE WINS!")
+            canvas.create_rectangle(440, 150, 1040, 294, fill="white", outline="#3270a9", width=10)
+            canvas.create_text(740, 222, text="BLUE WINS!", font="Arial 70 bold", fill="#3270a9")
 
-    canvas.update()
-    canvas.after(1)
+        else:
+            print("DRAW!")
+            canvas.create_rectangle(440, 150, 1040, 294, fill="white", outline="#3270a9", width=10)
+            canvas.create_text(740, 222, text="DRAW!", font="Arial 70 bold", fill="#ff393a")
 
-    if red_pos < 1250 or blue_pos < 1250:
-        canvas.delete(cas1)
-        canvas.delete(cas2)
-else:
-    if red_pos > blue_pos:
-        print("RED WINS!")
-        canvas.create_rectangle(440, 150, 1040, 294, fill="white", outline="#ff393a", width=10)
-        canvas.create_text(740, 222, text="RED WINS!", font="Arial 70 bold", fill="#ff393a")
+def reset(_):
+    global red_pos, blue_pos, time, track
 
-    elif red_pos < blue_pos:
-        print("BLUE WINS!")
-        canvas.create_rectangle(440, 150, 1040, 294, fill="white", outline="#3270a9", width=10)
-        canvas.create_text(740, 222, text="BLUE WINS!", font="Arial 70 bold", fill="#3270a9")
+    canvas.delete("all")
 
-    else:
-        print("DRAW!")
-        canvas.create_rectangle(440, 150, 1040, 294, fill="white", outline="#3270a9", width=10)
-        canvas.create_text(740, 222, text="DRAW!", font="Arial 70 bold", fill="#ff393a")
+    red_pos = 0
+    blue_pos = 0
+    time = 0
+
+    canvas.create_image(740, 222, image=track)
+    canvas.create_image(red_pos, 111, image=red, tags="red_car")
+    canvas.create_image(blue_pos, 333, image=blue, tags="blue_car")
+
+canvas.bind("<Button-1>", click)
+canvas.bind("<Button-3>", reset)
